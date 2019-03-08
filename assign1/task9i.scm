00d938cba7bb223bfb7dfd673ddd0426b1129ff2
Nick (Macbook) <nicksayswatzup@gmail.com>
2019-03-08 13:02:29 -0600

    task9i and prints latex right now

--- assign1/task9i.scm
+++ assign1/task9i.scm
@@ -1,22 +1,17 @@
 (define (ramanujani depth)
-    (define (helper source i store)
+    (define (helper source store)
         (cond 
-            ((= source 0) (sqrt store))
-            ; (else
-            ;     (sqrt (+ 
-            ;                 store
-            ;                 (* 
-            ;                     (+ i 1) 
-            ;                     (helper (- source 1) (+ i 1) (+ store 1))
-            ;                 )
-            ;             )
-            ;     )
-            ; )
+            ((= source 0) store)
+            (else (helper (- source 1) (* 
+                                    source 
+                                    (sqrt (+ (+ source 5) store))
+                                 )
+                  )
+            )
         )
     )
-    (helper depth 1 6)
+    (helper (+ depth 1) 0)
 )
-
 
 (define (print-raman depth)
     (define (helper source i string store)
@@ -46,5 +41,5 @@
     (setPort (open (getElement ScamArgs 1) 'read))
     (define arg1 (readExpr))
     (println "(ramanujani " arg1 ") is " (fmt "%.25f" (ramanujani arg1)))
-    (println (print-raman 0))
+    (println (print-raman arg1))
 )
