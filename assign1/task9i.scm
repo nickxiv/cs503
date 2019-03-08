8fae38eae0180fe4136561a79779a86658fc621d
Nick (Macbook) <nicksayswatzup@gmail.com>
2019-03-07 19:05:35 -0600

    latex expressions

--- assign1/task9i.scm
+++ assign1/task9i.scm
@@ -2,25 +2,49 @@
     (define (helper source i store)
         (cond 
             ((= source 0) (sqrt store))
-            (else
-                (sqrt (+ 
-                            store
-                            (* 
-                                (+ i 1) 
-                                (helper (- source 1) (+ i 1) (+ store 1))
-                            )
-                        )
-                )
-            )
+            ; (else
+            ;     (sqrt (+ 
+            ;                 store
+            ;                 (* 
+            ;                     (+ i 1) 
+            ;                     (helper (- source 1) (+ i 1) (+ store 1))
+            ;                 )
+            ;             )
+            ;     )
+            ; )
         )
     )
     (helper depth 1 6)
 )
 
 
+(define (print-raman depth)
+    (define (helper source i string store)
+        (cond 
+            ((= source 0) 
+                (append-braces (string+ string i " \\cdot \\sqrt{" store) depth)
+            )
+            (else
+                (helper (- source 1) (+ i 1) (string+ string i " \\cdot \\sqrt{" store " + ") (+ store 1))
+            )
+        )
+    )
+    (helper depth 1 "$" 6)
+)
+
+(define (append-braces string num)
+    (define (helper source store)
+        (cond 
+            ((= source 0) (string+ store "$"))
+            (else (helper (- source 1) (string+ store "}")))
+        )
+    )
+    (helper (+ num 1) string)
+)
 
 (define (main)
     (setPort (open (getElement ScamArgs 1) 'read))
     (define arg1 (readExpr))
     (println "(ramanujani " arg1 ") is " (fmt "%.25f" (ramanujani arg1)))
+    (println (print-raman 0))
 )
--- assign1/task9r.scm
+++ assign1/task9r.scm
@@ -17,10 +17,35 @@
     (helper depth 1 6)
 )
 
+(define (print-raman depth)
+    (define (helper source i string store)
+        (cond 
+            ((= source 0) 
+                (append-braces (string+ string i " \\cdot \\sqrt{" store) depth)
+            )
+            (else
+                (helper (- source 1) (+ i 1) (string+ string i " \\cdot \\sqrt{" store " + ") (+ store 1))
+            )
+        )
+    )
+    (helper depth 1 "$" 6)
+)
+
+(define (append-braces string num)
+    (define (helper source store)
+        (cond 
+            ((= source 0) (string+ store "$"))
+            (else (helper (- source 1) (string+ store "}")))
+        )
+    )
+    (helper (+ num 1) string)
+)
+
 
 
 (define (main)
     (setPort (open (getElement ScamArgs 1) 'read))
     (define arg1 (readExpr))
     (println "(ramanujanr " arg1 ") is " (fmt "%.25f" (ramanujanr arg1)))
+    (println (print-raman 0))
 )
