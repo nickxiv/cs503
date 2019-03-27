82f001b187402c1254b21c626eb329f309a9d3b1
Nick (Macbook) <nicksayswatzup@gmail.com>
2019-03-27 11:37:45 -0500

    task1 EZ Clap

--- assign2/task1.scm
+++ assign2/task1.scm
@@ -1,3 +1,28 @@
+(define (range a b step)
+    (cond 
+        ((>= a b) nil)
+        (else (cons a (range (+ a step) b step)))    
+    )
+)
+
+(define (for-loop vals f)
+    (cond 
+        ((nil? vals))
+        (else 
+            (f (car vals))
+            (for-loop (cdr vals) f)
+        )
+    )
+)
+
+
 (define (main)
-    (println "-1")
+    (setPort (open (getElement ScamArgs 1) 'read))
+    (define env this)
+    (define (iter expr)
+        (if (not (eof?)) 
+            (begin (eval expr env) (iter (readExpr)))
+        )
+    )
+    (iter (readExpr))    
 )
