56093ba1aa446a0c44372eb954362c9e7efc66f2
Nick (Macbook) <nicksayswatzup@gmail.com>
2019-03-31 14:02:16 -0500

    lmao task7

--- assign2/task7.scm
+++ assign2/task7.scm
@@ -1,3 +1,45 @@
+;all functions are sourced from or based upon functions and exercises found within SICP
+
+(define (accumulate op initial sequence)
+    (if (nil? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence))
+        )
+    )
+)
+
+(define (accumulate-n op init seqs)
+    (if (nil? (car seqs))
+        nil
+        (cons 
+            (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))
+    )
+)
+
+(define (dot-product v w)
+    (accumulate + 0 (map * v w))
+)
+
+(define (matrix-*-vector m v)
+    (map (lambda (x) (dot-product v x)) m)
+)
+
+(define (matrix-*-matrix m n)
+    (transpose (map (lambda(x) (matrix-*-vector n x)) (transpose m)))
+)
+
+(define (transpose mat) (accumulate-n cons nil mat))
+
+
 (define (main)
-    (println "-1")
+    (setPort (open (getElement ScamArgs 1) 'read))
+    (define env this)
+    (define (iter expr)
+        (if (not (eof?)) 
+            (begin (eval expr env) (iter (readExpr)))
+        )
+    )
+    (iter (readExpr))    
 )
